84e283a2adbc5c881704f5a5a89326d39182360f
kourosh karimi <karimikorosh2000@gmail.com>
2024-04-10 17:40:09 +0330

    Consuming API pt.2

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
+using ApiResponse = MagicVilla_VillaAPI.Models.ApiResponse;
+
+namespace MagicVilla_Web.Services;
+
+public class BaseService : IBaseService
+{
+    public APIRequest responseModel { get; set; }
+    public IHttpClientFactory HttpClient { get; set; }
+
+    public BaseService(IHttpClientFactory httpClient)
+    {
+        this.responseModel = new();
+        this.HttpClient = httpClient;
+    }
+
+    public async Task<T> SendAsync<T>(APIRequest apiRequest)
+    {
+        try
+        {
+            var client = HttpClient.CreateClient("MagicAPI");
+            HttpRequestMessage message = new HttpRequestMessage();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri(apiRequest.Url);
+            if (apiRequest.Data is not null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                    Encoding.UTF8, "application/json");
+            }
+
+            switch (apiRequest.ApiType)
+            {
+                case SD.ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+
+                case SD.ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+
+                case SD.ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+
+                default:
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+
+            HttpResponseMessage apiResponse = null;
+            apiResponse = await client.SendAsync(message);
+
+            var apiContent = await apiResponse.Content.ReadAsStringAsync();
+            var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+            return APIResponse;
+        }
+        catch (Exception e)
+        {
+            var dto = new ApiResponse
+            {
+                ErrorMessage = new List<string> { Convert.ToString(e.Message) },
+                IsSuccess = false
+            };
+            var res = JsonConvert.SerializeObject(dto);
+            var APIResponse = JsonConvert.DeserializeObject<T>(res);
+            return APIResponse;
+        }
+    }
+}
---
+++ MagicVilla_Web/Services/IServices/IVillaService.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaAPI.Models.DTO;
+
+namespace MagicVilla_Web.Services.IServices;
+
+public interface IVillaService
+{
+    Task<T> GetAll<T>();
+    Task<T> Get<T>(int id);
+    Task<T> Create<T>(VillaCreateDTO dto);
+    Task<T> Update<T>(VillaUpdateDTO dto);
+    Task<T> Delete<T>(int id);
+}
