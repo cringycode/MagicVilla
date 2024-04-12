cd41e74d19978a87a029ae3129b9315b53793acd
kourosh karimi <karimikorosh2000@gmail.com>
2024-04-12 19:06:19 +0330

    Adding AutoMapper.

---
+++ MagicVilla_Web_Fixed/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MagicVilla_Web_Fixed.Models.DTO;
+
+namespace MagicVilla_Web_Fixed;
+
+public class MappingConfig : Profile
+{
+    public MappingConfig()
+    {
+        CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+        CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+        
+        CreateMap<VillaNumberDTO, VillaCreateDTO>().ReverseMap();
+        CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+    }
+}
