ec0d207536ad35bca4b71dc045bf1f330afcb456
kourosh karimi <karimikorosh2000@gmail.com>
2024-04-12 18:52:37 +0330

    API Requset & DTO's.

---
+++ MagicVilla_Web_Fixed/Models/APIRequest.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_Utility_Fixed;
+
+namespace MagicVilla_Web_Fixed.Models;
+
+public class APIRequest
+{
+    public SD.ApiType ApiType { get; set; } = SD.ApiType.GET;
+    public string Url { get; set; }
+    public object Data { get; set; }
+}
